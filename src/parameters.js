3abf361b8bfbb4164bc9fc60a37d496cfb92c324
zhoucheng <zhoucheng133@outlook.com>
2024-04-07 15:31:37 +0800

    first commit

---
+++ src/parameters.js
@@ -0,0 +1,12 @@
+// 这里存放一些参数，务必根据注释修改下面的内容
+
+// aira2的rpc地址，例如 http://192.168.1.3:16800/jsonrpc
+const aria2_link="";
+
+// aria2的rpc连接密钥，如果没有则保持空
+const aria2_secret="";
+
+// 下载规则，注意下面的规则都是并列的
+const rules={
+    
+}
